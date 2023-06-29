e88d05bab0c65a5eebc4e4a401d5fa8533c19d52
ErickBut <erickbutron1@gmail.com>
2023-06-29 18:22:42 -0400

    adding responsive

--- my-blog-app/src/app/layout.js
+++ my-blog-app/src/app/layout.js
@@ -1,11 +1,14 @@
+import NextHead from 'next/head';
 import './styles.css';
+import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <NextHead>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" />
-      </head>
+      </NextHead>
       <body className="bg-gray-100">
         {children}
       </body>
